f332766784fb2bdc1fd069333d8ee1a24d8c577c
김민선 <minsun9856@gmail.com>
2026-02-11 09:44:23 +0900

    feat: 대시보드 아이콘 추가 및 반응형 개선

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { usePens } from "@/hooks/usePens";
-import { ChevronDown, ChevronRight } from "lucide-react";
+import {
+  Activity,
+  ChevronDown,
+  ChevronRight,
+  Clock4,
+  PiggyBank,
+  Thermometer,
+} from "lucide-react";
 
 export default function DashboardPage() {
   const { data, isLoading, isError } = usePens();
@@ -55,59 +62,71 @@ export default function DashboardPage() {
                   className="bg-white border border-gray-200 rounded-lg cursor-pointer"
                 >
                   <div className="flex flex-row items-center p-4">
-                    <span className="ml-2 mr-6">
+                    <span className="lg:ml-2 lg:mr-6 mr-2">
                       {isOpen ? (
-                        <ChevronDown size={24} />
+                        <ChevronDown size={22} />
                       ) : (
-                        <ChevronRight size={24} className="text-gray-400" />
+                        <ChevronRight size={22} className="text-gray-400" />
                       )}
                     </span>
-                    <div className="flex flex-row justify-between w-full gap-30">
+                    <div className="flex flex-col lg:flex-row justify-between w-full gap-4 lg:gap-30">
                       <div className="flex flex-row gap-2 items-center">
                         <span className="font-bold text-lg">
                           {pen.pen_name}
                         </span>
-                        <span className="text-white bg-red-600 px-3 py-1.5 rounded-full text-xs">
+                        <span className="text-white bg-red-600 px-3 py-1.5 rounded-full text-sm">
                           {pen.abnormal_pigs.length}두
                         </span>
                       </div>
-                      <div className="grid grid-cols-4 flex-1">
-                        <div className="flex flex-col gap-1">
-                          <span>재고</span>
-                          <span className="font-semibold">
-                            {pen.current_pig_count}두
-                          </span>
+                      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 flex-1">
+                        <div className="flex flex-row items-center gap-2 lg:gap-4">
+                          <PiggyBank className="w-10 h-10 shrink-0 bg-[#F6FAFF] text-[#062454] rounded-full p-2" />
+                          <div className="flex flex-col gap-1">
+                            <span>재고</span>
+                            <span className="font-semibold truncate">
+                              {pen.current_pig_count}두
+                            </span>
+                          </div>
                         </div>
-                        <div className="flex flex-col gap-1">
-                          <span>활력도</span>
-                          <span className="font-semibold">
-                            {pen.avg_activity_level}m
-                          </span>
+                        <div className="flex flex-row items-center gap-2 lg:gap-4">
+                          <Activity className="w-10 h-10 shrink-0 bg-[#F6FAFF] text-[#062454] rounded-full p-2" />
+                          <div className="flex flex-col gap-1">
+                            <span>활력도</span>
+                            <span className="font-semibold truncate">
+                              {pen.avg_activity_level}m
+                            </span>
+                          </div>
                         </div>
-                        <div className="flex flex-col gap-1">
-                          <span>식사 시간</span>
-                          <span className="font-semibold">
-                            {pen.avg_feeding_time_minutes}분
-                          </span>
+                        <div className="flex flex-row items-center gap-2 lg:gap-4">
+                          <Clock4 className="w-10 h-10 shrink-0 bg-[#F6FAFF] text-[#062454] rounded-full p-2" />
+                          <div className="flex flex-col gap-1">
+                            <span>식사 시간</span>
+                            <span className="font-semibold truncate">
+                              {pen.avg_feeding_time_minutes}분
+                            </span>
+                          </div>
                         </div>
-                        <div className="flex flex-col gap-1">
-                          <span>온도</span>
-                          <span className="font-semibold">
-                            {pen.avg_temperature_celsius}℃
-                          </span>
+                        <div className="flex flex-row items-center gap-2 lg:gap-4">
+                          <Thermometer className="w-10 h-10 shrink-0 bg-[#F6FAFF] text-[#062454] rounded-full p-2" />
+                          <div className="flex flex-col gap-1">
+                            <span>온도</span>
+                            <span className="font-semibold truncate">
+                              {pen.avg_temperature_celsius}℃
+                            </span>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                   {isOpen && pen.abnormal_pigs.length > 0 && (
                     <>
-                      <hr className="border-gray-300" />
+                      <hr className="border-gray-200" />
                       <div className="p-6">
                         <p className="mb-4 text-gray-500 font-semibold">
                           이상 개체 목록 ({pen.abnormal_pigs.length}구)
                         </p>
 
-                        <div className="grid grid-cols-2 gap-2">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
                           {pen.abnormal_pigs.map((pig) => (
                             <div
                               key={pig.wid}
@@ -117,15 +136,21 @@ export default function DashboardPage() {
                                 <img
                                   src={pig.thumbnail_url}
                                   alt="pig"
-                                  className="w-18 h-18 object-cover rounded-md"
+                                  className="w-16 h-16 object-cover rounded-md"
                                 />
                                 <p className="text-red-600 font-bold text-xl">
                                   {pig.wid}
                                 </p>
                               </div>
-                              <div className="w-3/4 flex flex-row justify-between font-semibold">
-                                <p>{pig.activity}m</p>
-                                <p>{pig.feeding_time}분</p>
+                              <div className="w-3/5 flex flex-row justify-between font-semibold">
+                                <div className="flex flex-row items-center gap-2 lg:gap-4">
+                                  <Activity className="w-10 h-10 shrink-0 bg-[#F6FAFF] text-[#062454] rounded-full p-2" />
+                                  <span>{pig.activity}m</span>
+                                </div>
+                                <div className="flex flex-row items-center gap-2 lg:gap-4">
+                                  <Clock4 className="w-10 h-10 shrink-0 bg-[#F6FAFF] text-[#062454] rounded-full p-2" />
+                                  <span>{pig.feeding_time}분</span>
+                                </div>
                                 <p></p>
                               </div>
                             </div>
